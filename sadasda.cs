ecaa31fd71d72f3ca4d74fcbdfae49efc347d143
TTaTRiARX <radetskii@dev-game.ru>
2023-10-17 16:30:40 +0300

    ttcommit

---
+++ sadasda.cs
@@ -0,0 +1,11 @@
+static void main()
+ {
+int a, b , sum;
+a = convert.toint32(console.readline());
+console.write("vvedite pervoe chislo: ")
+b = convert.toint32(console.readline());
+console.write("vvedite vtoroe chislo: ")
+sum = a + b;
+console.write("summa chisel ravna: $"{sum}" ");
+console.readline();
+}
